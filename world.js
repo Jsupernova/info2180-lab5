d612dc10e9e3035a9c3c79d1b1e405aed2bfd886
Jsupenova <jasond1andonly@gmail.com>
2020-11-19 12:49:54 -0500

    Display cities in a country

--- world.js
+++ world.js
@@ -36,5 +36,34 @@ function loadrequest()
         httpRequest.send();
 
     }
+    document.getElementById("citylookup").onclick= function()
+    {
+        var httpRequest = new XMLHttpRequest();
+        var url = "http://localhost/info2180-lab5/world.php?";
+        
+        
+        const data = "country=" + document.getElementById("country").value+"&context=cities";
+         httpRequest.onreadystatechange = function()
+        {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) 
+            {
+                if (httpRequest.status === 200) 
+                {
+                    
+                        var response = httpRequest.responseText;
+                    
+                    document.getElementById("result").innerHTML= response;
+                    
+                    
+                 } 
+            }
+        };
 
+        httpRequest.open('GET', url+data,true);
+        event.preventDefault();
+
+        
+        httpRequest.send();
+
+    }
 }
--- world.php
+++ world.php
@@ -13,32 +13,46 @@ if($context=="cities"){
    
 $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE \"%$country%\" ORDER BY countries.name ASC");
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        print($results) ;
  }else{
      $countrieswhere = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
      $whereresults= $countrieswhere->fetchAll(PDO::FETCH_ASSOC);
  }
 }
 ?>
+<table>
 
- <table>
-   
-    
 
-        <th> Country Name</th>
-        <th> Continent</th>
-        <th> Independence Year</th>
-        <th> Head of State</th>
-   
-    <?php foreach ($whereresults as $row): ?>
-        <tr>
-            <td><?= $row['name']; ?></td>
-            <td><?= $row['continent']; ?></td>
-            <td><?=$row['independence_year']; ?></td>
-            <td><?= $row['head_of_state']; ?> </td>
-        </tr>
-    <?php endforeach; ?>
+    <?php if($context=="cities"): ?>
+     
+            <th> Country Name</th>
+            <th> District</th>
+            <th> Population</th>
+            
+        
+            <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['district']; ?></td>
+                <td><?=$row['population']; ?></td>
+            </tr>
+        <?php endforeach; ?>
 
-   
- </table>
+    <?php else: ?>
+     
+            <th> Country Name</th>
+            <th> Continent</th>
+            <th> Independence Year</th>
+            <th> Head of State</th>
+       
+        <?php foreach ($whereresults as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['continent']; ?></td>
+                <td><?=$row['independence_year']; ?></td>
+                <td><?= $row['head_of_state']; ?> </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+
+</table>
 <?php header('Access-Control-Allow-Origin: *');?>
